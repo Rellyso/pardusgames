e129b49e916cc0af0121341b817fdb321c4b8985
Rellyson Douglas <rellysondouglas2015@gmail.com>
2024-05-16 20:10:40 -0300

    feat: add smooth scroll with Lenis

--- providers/app-provider.tsx
+++ providers/app-provider.tsx
@@ -1,6 +1,7 @@
 'use client'
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
+import { ReactLenis } from 'lenis/react'
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
@@ -9,5 +10,9 @@ if (typeof window !== 'undefined') {
 }
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+  return (
+    <ReactLenis root>
+      <PostHogProvider client={posthog}>{children}</PostHogProvider>
+    </ReactLenis>
+  )
 }
