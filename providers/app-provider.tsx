8efcebf43f1954d05d60fdad3a79676b52931547
Rellyson Douglas <rellysondouglas2015@gmail.com>
2024-05-17 13:54:29 -0300

    Revert "feat: add smooth scroll with Lenis"
    This reverts commit e129b49e916cc0af0121341b817fdb321c4b8985.

--- providers/app-provider.tsx
+++ providers/app-provider.tsx
@@ -1,7 +1,6 @@
 'use client'
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
-import { ReactLenis } from 'lenis/react'
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
@@ -10,9 +9,5 @@ if (typeof window !== 'undefined') {
 }
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <ReactLenis root>
-      <PostHogProvider client={posthog}>{children}</PostHogProvider>
-    </ReactLenis>
-  )
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
 }
